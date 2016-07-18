ec9ed070ac519202d3161488d6c51c5449045a30
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2016-07-18 11:07:24 -0500

    Updates

--- ImageStreamWebServer/src/imagestream/IImage.java
+++
@@ -1,9 +0,0 @@
-package imagestream;
-
-/**
- * Defines a platform-independent Image interface, which can be
- * implemented for different runtime environments, e.g., Android or
- * plain Java.
- */
-interface IImage {
-}
