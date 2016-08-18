ebf4bec5f5d70b600bbba7f4fc609c355a33054d
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2016-08-18 11:32:33 -0500

    Updates

--- SearchStreamGang/src/main/java/livelessons/streamgangs/SearchWithRxJavaInputs.java
+++ SearchStreamGang/src/main/java/livelessons/streamgangs/SearchWithRxJavaInputs.java
@@ -25,7 +25,7 @@ import static java.util.stream.Collectors.toList;
  * word in an array of words.
  */
 public class SearchWithRxJavaInputs
-    extends SearchStreamGang {
+       extends SearchStreamGang {
     /**
      * Constructor initializes the super class.
      */
@@ -37,56 +37,79 @@ public class SearchWithRxJavaInputs
     }
 
     /**
-     * Perform the processing, which uses a Java 8 Stream to
+     * Perform the processing, which uses an RxJava Stream to
      * concurrently search for words in the input data.
      */
     @Override
     protected List<List<SearchResults>> processStream() {
-        // Convert the input strings into a list of futures.
-        List<List<SearchResults>> result = new ArrayList<>();
+        return Observable
+            // Converts input strings array into an Observable that
+            // emits the items in the array.
+            .from(getInput())
 
-        Observable.from(getInput())
-                  .map(this::processInputAsync)
+           // Returns an Observable that emits items based on applying
+           // processInput() to each item emitted by the source
+           // Observable, where processInput() returns an Observable,
+           // and then merging the resulting Observables and emitting
+           // the results of this merger as a single Observable.
+            .flatMap(inputString ->
+                     Observable
 
-                  // @@ Monte, should the call to flatMap() be moved
-                  // to a separate statement, i.e., *after* creating
-                  // the list of Observables?
-                  .flatMap(t -> t)
+                     // Returns an Observable that emits a single
+                     // input string and then completes.
+                     .just(inputString)
 
-                  // @@ Monte, is there any way to avoid using
-                  // toBlocking() here since that may cause additional
-                  // synchronization overhead?
-                  .toBlocking()
+                     // Asynchronously subscribes Observers to this
+                     // Observable on the computation scheduler.
+                     .subscribeOn(Schedulers.computation())
 
-                  // @@ Monte, could toList() be used here instead of forEach()?
-                  .forEach(result::add);
+                     // Returns an Observable that applies the
+                     // processInput() method to each item emitted by
+                     // the source Observable and emits a list of
+                     // SearchResults.
+                     .map(this::processInput))
 
-        return result;
+           // Returns an Observable that emits a single item: a list
+           // composed of all the items emitted by the source
+           // Observable, which is itself a list of SearchResults.
+           .toList()
+
+           // Converts an Observable into a BlockingObservable (an
+           // Observable with blocking operators).
+           .toBlocking()
+
+           // Returns an Observable that emits only the very first
+           // item emitted by the source Observable, which is a list
+           // of a list of SearchResults.
+           .first();
     }
 
     /**
      * Search input string for all occurrences of the words to find.
      */
-    private Observable<List<SearchResults>> processInputAsync(String inputString) {
-        // Store the title.
+    private List<SearchResults> processInput(String inputString) {
+        // Get the title.
         String title = getTitle(inputString);
 
         // Skip over the title.
         String input = inputString.substring(title.length());
 
-        return Observable.from(mWordsToFind)
-                         // @@ Monte, should the call to flatMap() be
-                         // moved to a separate statement, i.e.,
-                         // *after* creating the list of Observables?
-                         .flatMap(word -> Observable.<SearchResults>create(subscriber -> {
-                                     // @@ Monte, do we need the intermediate variable or
-                                     // can we just say:
-                                     // subscriber.onNext(searchForWord(word, input, title));
-                                     SearchResults searchResults = searchForWord(word, input, title);
-                                     subscriber.onNext(searchResults);
-                                     subscriber.onCompleted();
-                                 }).subscribeOn(Schedulers.computation()))
-                         .toList();
+        return mWordsToFind
+            // Sequentially process each word.
+            .stream()
+
+            // Map each word to a stream of SearchResults for each
+            // time the word is found in the input.
+            .map(word -> 
+                 // Return a SearchResults containing a match for
+                 // each time word appears in the input string.
+                 searchForWord(word, input, title))
+
+            // Only keep SearchResults with at least one match.
+            .filter(result -> result.size() > 0)
+
+            // Create a list of SearchResults.
+            .collect(toList());
     }
 }
 
--- SearchStreamGang/src/main/java/livelessons/streamgangs/SearchWithRxJavaWords.java
+++ SearchStreamGang/src/main/java/livelessons/streamgangs/SearchWithRxJavaWords.java
@@ -7,6 +7,7 @@ import java.util.concurrent.CompletableFuture;
 import livelessons.utils.SearchResults;
 import livelessons.utils.StreamsUtils;
 import rx.Observable;
+import rx.observables.BlockingObservable;
 import rx.schedulers.Schedulers;
 
 import static java.util.stream.Collectors.toList;
@@ -28,48 +29,77 @@ public class SearchWithRxJavaWords
                 stringsToSearch);
     }
 
+    /**
+     * Perform the processing, which uses an RxJava Stream to
+     * concurrently search for words in the input data.
+     */
    @Override
-    protected List<List<SearchResults>> processStream() {
+   protected List<List<SearchResults>> processStream() {
        return Observable
-           // Get the input from the list of words.
+           // Converts mWordsToFind array into an Observable that
+           // emits the items in the array.
            .from(mWordsToFind)
 
-           // ...
-           .flatMap(val -> 
-                    // ...
-                    Observable.just(val)
-                    // ...
+           // Returns an Observable that emits items based on applying
+           // processWord() to each item emitted by the source
+           // Observable, where processWord() returns an Observable,
+           // and then merging the resulting Observables and emitting
+           // the results of this merger as a single Observable.
+           .flatMap(word ->
+                    Observable
+                    // Returns an Observable that emits a single word
+                    // and then completes.
+                    .just(word)
+
+                    // Asynchronously subscribes Observers to this
+                    // Observable on the computation scheduler.
                     .subscribeOn(Schedulers.computation())
-                    // ...
+
+                    // Returns an Observable that applies the
+                    // processWord() method to each item emitted by
+                    // the source Observable and emits a list of
+                    // SearchResults.
                     .map(this::processWord))
 
-           // ...
+           // Returns an Observable that emits a single item: a list
+           // composed of all the items emitted by the source
+           // Observable, which is itself a list of SearchResults.
            .toList()
 
-           // ...
+           // Converts an Observable into a BlockingObservable (an
+           // Observable with blocking operators).
            .toBlocking()
 
-           // ...
+           // Returns an Observable that emits only the very first
+           // item emitted by the source Observable, which is a list
+           // of a list of SearchResults.
            .first();
    }
 
+    /**
+     * Search all the input strings for all occurrences of the word to
+     * find.
+     */
     private List<SearchResults> processWord(String word) {
      	// Get the input.
         return getInput()
             // Sequentially process each String in the input list.
             .stream()
 
-            // Map each String to a Stream containing the words found
-            // in the input.
-            .map(inputSTring -> {
+            // Map each input string to a stream of SearchResults for
+            // each time the word appears in the input.
+            .map(inputString -> {
                     String title = getTitle(inputString);
+
+                    // Return a SearchResults containing a match for
+                    // each time word appears in the input string.
                     return searchForWord(word, 
                                          // Skip over the title.
-                                         input.substring(title.length()),
+                                         inputString.substring(title.length()),
                                          title);
                 })
 
-            // Only keep a result that has at least one match.
+            // Only keep SearchResults with at least one match.
             .filter(result -> result.size() > 0)
 
             // Create a list of SearchResults.
