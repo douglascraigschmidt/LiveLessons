faa4902fa79804c1919352e0ac0ae1f8e52c9261
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2017-01-02 19:04:48 -0600

    Updats

--- Java8/ex1/src/ex1.java
+++ Java8/ex1/src/ex1.java
@@ -9,23 +9,6 @@ import java.util.stream.Stream;
  */
 public class ex1 {
     static public void main(String[] argv) {
-    	ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
-    	map.put("foo", "bar");
-    	map.put("han", "solo");
-    	map.put("r2", "d2");
-    	map.put("c3", "p0");
-    	
-    	String result = map.searchEntries(1, entry -> {
-    	    if (entry.getValue().length() > 3) {
-    	    	entry.setValue("brinker");
-    	        return entry.getKey();
-    	    }
-    	    return null;
-    	});
-
-    	System.out.println("Result: " + result + " result 2: " + map.get("han"));
-    	
-    	/*
         // The array to sort and print.
         String[] nameArray = {
             "Barbara",
@@ -39,10 +22,10 @@ public class ex1 {
             "mary"
         };
 
+        // Demonstrate the various techniques.
         showInnerClass(nameArray);
         showLambdaExpression(nameArray);
         showMethodReference(nameArray);
-        */
     }
 
     /**
