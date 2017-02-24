09efb24beb8721e9e69facae7b58cb0adbcec67c
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2017-02-24 14:31:54 -0600

    Updates

---
+++ BusySynchronizedQueue/src/main/java/edu/vandy/BusySynchronizedQueue.java
@@ -0,0 +1,121 @@
+package edu.vandy;
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
+/*
+ * Implements the BoundedQueue interface that works properly when
+ * accessed via multiple threads since it's synchronized properly, but
+ * is inefficient since due to its "busy waiting".
+ */
+class BusySynchronizedQueue<E> 
+      implements BoundedQueue<E> {
+    /**
+     * The queue consists of a LinkedList of E's.
+     */
+    private List<E> mList = new LinkedList<>();
+
+    /**
+     * The maximum capacity of the queue or Integer.MAX_VALUE if none.
+     */
+    private final int mCapacity;
+
+    /**
+     * Create a SimpleBlocking queue with a capacity of
+     * Integer.MAX_VALUE.
+     */
+    public BusySynchronizedQueue() {
+        this(Integer.MAX_VALUE);
+    }
+
+    /**
+     * Create a SimpleBlocking queue with the given capacity.
+     */
+    public BusySynchronizedQueue(int capacity) {
+        if (capacity <= 0) 
+            throw new IllegalArgumentException();
+        mCapacity = capacity;
+        mList = new LinkedList<>();
+    }
+
+    /**
+     * Returns <tt>true</tt> if this queue contains no elements, else false.
+     *
+     * @return <tt>true</tt> if this queue contains no elements, else false.
+     */
+    @Override
+    public boolean isEmpty() {
+        synchronized(this) {
+            return mList.size() == 0;
+        }
+    }
+
+    /**
+     * Returns <tt>true</tt> if this queue is full, else false.
+     *
+     * @return <tt>true</tt> if this queue is full, else false.
+     */
+    @Override
+    public boolean isFull() {
+        synchronized(this) {
+            return mList.size() == mCapacity;
+        }
+    }
+
+    /**
+     * Returns the number of elements in this queue.
+     *
+     * @return the number of elements in this collection
+     */
+    @Override
+    public int size() {
+        synchronized(this) {
+            return mList.size();
+        }
+    }
+
+    /**
+     * Retrieves and removes the head of this queue, or returns {@code
+     * null} if this queue is empty.
+     *
+     * @return the head of this queue, or {@code null} if this queue is empty
+     */
+    @Override
+    public E poll() {
+        synchronized(this) {
+            if (!isEmpty())
+                return mList.remove(0);
+            else
+                return null;
+        }
+    }
+
+    /**
+     * Inserts the specified element into this queue if it is possible to do
+     * so immediately without violating capacity restrictions, returning
+     * {@code true} upon success and {@code false} if no space is currently
+     * available.
+     *
+     * @return {@code true} if the element was added to this queue, else
+     *         {@code false}
+     */
+    @Override
+    public boolean offer(E e) {
+        synchronized (this) {
+            if (!isFull()) {
+                mList.add(e);
+                return true;
+            } else
+                return false;
+        }
+    }
+}
+
+
+
+
+
+
---
+++ SimpleBlockingQueue/src/main/java/edu/vandy/SimpleBlockingBoundedQueue.java
@@ -0,0 +1,126 @@
+package edu.vandy;
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
+/*
+ * Defines an implementation of the BoundedQueue interface that works
+ * properly when accessed via multiple threads since it's synchronized
+ * properly.
+ */
+class SimpleBlockingBoundedQueue<E>
+      implements BoundedQueue<E> {
+    /**
+     * The queue consists of a LinkedList of E's.
+     */
+    final private List<E> mList;
+
+    /**
+     * The maximum capacity of the queue or Integer.MAX_VALUE if none.
+     */
+    private final int mCapacity;
+
+    /**
+     * Create a SimpleBlocking queue with a capacity of
+     * Integer.MAX_VALUE.
+     */
+    public SimpleBlockingBoundedQueue() {
+        this(Integer.MAX_VALUE);
+    }
+
+    /**
+     * Create a SimpleBlocking queue with the given capacity.
+     */
+    public SimpleBlockingBoundedQueue(int capacity) {
+        if (capacity <= 0) 
+            throw new IllegalArgumentException();
+        mCapacity = capacity;
+        mList = new LinkedList<>();
+    }
+
+    /**
+     * Inserts the specified element into this queue, waiting if
+     * necessary for space to become available.
+     *
+     * @param e the element to add
+     * @throws InterruptedException if interrupted while waiting
+     */
+    @Override
+    public void put(E e)
+        throws InterruptedException {
+        synchronized(this) {
+            if (e == null)
+                throw new NullPointerException();
+
+            // Wait until the queue is not full.
+            while (isFull()) {
+                // System.out.println("BLOCKING ON PUT()");
+                wait();
+            }
+
+            // Add e to the LinkedList.
+            mList.add(e);
+            
+            // Notify that the queue may have changed state, e.g., "no
+            // longer empty".
+            notifyAll();
+        }
+    } 
+
+    /**
+     * Retrieves and removes the head of this queue, waiting if necessary
+     * until an element becomes available.
+     *
+     * @return the head of this queue
+     * @throws InterruptedException if interrupted while waiting
+     */
+    @Override
+    public E take() throws InterruptedException {
+        synchronized(this) {
+            // Wait until the queue is not empty.
+            while (mList.isEmpty()) {
+                // System.out.println("BLOCKING ON TAKE()");
+                wait();
+            }
+
+            final E e = mList.remove(0);
+        
+            // Notify that the queue may have changed state, e.g., "no
+            // longer full".
+            notifyAll();
+            return e;
+        }
+    } 
+
+    /**
+     * Returns true if the queue is empty, else false.
+     */
+    @Override
+    public boolean isEmpty() {
+        synchronized(this) {
+            return mList.size() == 0;
+        }
+    }
+
+    /**
+     * Returns true if the queue is full, else false.  Since this
+     * isn't a public method it assumes the monitor lock is held.
+     */
+    @Override
+    public boolean isFull() {
+        return mList.size() == mCapacity;
+    }
+
+    /**
+     * Returns the number of elements in this queue.
+     */
+    @Override
+    public int size() {
+        synchronized(this) {
+            return mList.size();
+        }
+    }
+}
