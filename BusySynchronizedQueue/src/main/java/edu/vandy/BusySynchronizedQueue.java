7b67bbfa075172a2478456a42bc52daf05213498
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2017-02-26 14:02:01 -0600

    updates

--- BusySynchronizedQueue/src/main/java/edu/vandy/BusySynchronizedQueue.java
+++ BusySynchronizedQueue/src/main/java/edu/vandy/BusySynchronizedQueue.java
@@ -46,10 +46,8 @@ class BusySynchronizedQueue<E>
      * @return <tt>true</tt> if this queue contains no elements, else false.
      */
     @Override
-    public boolean isEmpty() {
-        synchronized(this) {
-            return mList.size() == 0;
-        }
+    public synchronized boolean isEmpty() {
+        return mList.size() == 0;
     }
 
     /**
@@ -58,10 +56,8 @@ class BusySynchronizedQueue<E>
      * @return <tt>true</tt> if this queue is full, else false.
      */
     @Override
-    public boolean isFull() {
-        synchronized(this) {
-            return mList.size() == mCapacity;
-        }
+    public synchronized boolean isFull() {
+        return mList.size() == mCapacity;
     }
 
     /**
@@ -70,10 +66,8 @@ class BusySynchronizedQueue<E>
      * @return the number of elements in this collection
      */
     @Override
-    public int size() {
-        synchronized(this) {
-            return mList.size();
-        }
+    public synchronized int size() {
+        return mList.size();
     }
 
     /**
@@ -83,10 +77,8 @@ class BusySynchronizedQueue<E>
      * @return the head of this queue, or {@code null} if this queue is empty
      */
     @Override
-    public E poll() {
-        synchronized(this) {
-            mList.poll();
-        }
+    public synchronized E poll() {
+        mList.poll();
     }
 
     /**
@@ -99,14 +91,12 @@ class BusySynchronizedQueue<E>
      *         {@code false}
      */
     @Override
-    public boolean offer(E e) {
-        synchronized (this) {
-            if (!isFull()) {
-                mList.add(e);
-                return true;
-            } else
-                return false;
-        }
+    public synchronized boolean offer(E e) {
+        if (!isFull()) {
+            mList.add(e);
+            return true;
+        } else
+            return false;
     }
 }
 
