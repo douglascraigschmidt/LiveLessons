997dcbeec1eff8faa0c4f6314a3f3d18286718c1
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2017-10-26 21:46:39 -0500

    updates

--- SearchStreamForkJoin/RecursiveTraversalStreams/src/main/java/search/SearchWithForkJoinTask.java
+++ SearchStreamForkJoin/RecursiveTraversalStreams/src/main/java/search/SearchWithForkJoinTask.java
@@ -3,7 +3,9 @@ package search;
 import utils.Document;
 import utils.Folder;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ForkJoinTask;
 import java.util.concurrent.RecursiveTask;
 import java.util.stream.Stream;
 
@@ -63,7 +65,7 @@ public class SearchWithForkJoinTask
      * phrases in a given work of Shakespeare.
      */
     class WorkSearchTask
-        extends RecursiveTask<List<SearchResults>> {
+          extends RecursiveTask<List<SearchResults>> {
         /**
          * The recursive directory folder containing a particular work
          * of Shakespeare.
@@ -88,80 +90,36 @@ public class SearchWithForkJoinTask
                 .getSubFolders();
             
             // Start by adding the intro document.
-            List<Document> docs = mWork.getDocuments();
+            List<Document> docs = 
+                mWork.getDocuments();
 
             // Then add all the act documents if there are any.
             if (docsFolder.size() > 0)
                 docs.addAll(docsFolder.get(0).getDocuments());
             
-            // Create a stream for all the documents to search.
-            Stream<Document> docsStream = docs
-                .stream();
+            // The list of fork-join tasks to run in parallel.
+            List<ForkJoinTask<List<SearchResults>>> forks =
+                new ArrayList<>();
 
-            // Convert to a parallel stream if requested.
-            if (mParallelInput)
-                docsStream.parallel();
+            // The list of results.
+            List<SearchResults> results = 
+                new ArrayList<>();
 
-            /*
-            return docsStream
-                // Convert each Document to a SearchForPhrasesTask and
-                // fork it to run in parallel.
-                .map(doc
-                     -> new SearchForPhrasesTask(mWork.getName(),
-                                                 doc.getContents(),
-                                                 mPhrasesToFind,
-                                                 mParallelSearching,
-                                                 mParallelPhrases).fork())
+            for (Document doc : mWork.getDocuments())
+                 // Convert each Document to a SearchForPhrasesTask and
+                 // fork it to run in parallel.
+                 forks.add(new SearchForPhrasesTask(mWork.getName(),
+                                                    doc.getContents(),
+                                                    mPhrasesToFind,
+                                                    mParallelSearching,
+                                                    mParallelPhrases).fork());
 
-                // Join all the SearchForPhrasesTask results.
-                .map(ForkJoinTask::join)
+            // Join all the results.
+            for (ForkJoinTask<List<SearchResults>> task : forks)
+                results.addAll(task.join());
 
-                // Flatten the results of join() into a single stream
-                // containing all the search results for this work.
-                .flatMap(List::stream)
-
-                // Trigger intermediate operations and return a list.
-                .collect(toList());
-            */
-
-            // Create a list of all the tasks that search for phrases
-            // in the docs comprising a work.
-            List<SearchForPhrasesTask> docsList = docsStream
-                // Convert each Document object to a
-                // SearchForPhrasesTask object.
-                .map(doc
-                     -> new SearchForPhrasesTask(mWork.getName(),
-                                                 doc.getContents(),
-                                                 mPhrasesToFind,
-                                                 mParallelSearching,
-                                                 mParallelPhrases))
-
-                // Trigger intermediate operation processing and
-                // return a list.
-                .collect(toList());
-
-            Stream<SearchForPhrasesTask> resultsStream = invokeAll(docsList)
-                // Convert the list into a stream.
-                .stream();
-
-            if (mParallelInput)
-                resultsStream.parallel();
-
-            // Return a list of SearchResults corresponding to this
-            // work.  The invokeAll() method forks all the tasks in
-            // the docsList and returns a collection once all the
-            // tasks are done.
-            return resultsStream
-                // Join all the SearchForPhrasesTask results.
-                .map(RecursiveTask::join)
-
-                // Flatten the results of join() into a single stream
-                // containing all the search results for this work.
-                .flatMap(List::stream)
-
-                // Trigger intermediate operation processing and
-                // return a list.
-                .collect(toList());
+            // Return the results.
+            return results;
         }
     }
 
@@ -170,7 +128,7 @@ public class SearchWithForkJoinTask
      * of the given phrases.
      */
     class MasterSearchTask
-        extends RecursiveTask<List<List<SearchResults>>> {
+          extends RecursiveTask<List<List<SearchResults>>> {
         /**
          * The recursive directory folder containing the complete
          * works of Shakespeare.
@@ -193,61 +151,29 @@ public class SearchWithForkJoinTask
          */
         @Override
         protected List<List<SearchResults>> compute() {
+            // The list of fork-join tasks to run in parallel.
+            List<ForkJoinTask<List<SearchResults>>> forks =
+                new ArrayList<>();
+
+            // The list of results.
+            List<List<SearchResults>> results = 
+                new ArrayList<>();
+                
             // Create a stream containing all the works of
             // Shakespeare.
-            Stream<Folder> worksStream = mRootFolder
-                // Get a list of all the folders in the root
-                // directory.
-                .getSubFolders()
+            for (Folder work : mRootFolder
+                     // Get a list of all folders in root directory.
+                     .getSubFolders())
+                // Map each work in the folder to a WorkSearchTask and
+                // fork it to run in parallel.
+                forks.add(new WorkSearchTask(work).fork());
 
-                // Conver the list into a stream.
-                .stream();
+            // Join all the results.
+            for (ForkJoinTask<List<SearchResults>> task : forks)
+                results.add(task.join());
 
-            // Convert to a parallel stream if requested.
-            if (mParallelInput)
-                worksStream.parallel();
-
-            /*
-            // Return a list of lists of search results - one list for
-            // each work of Shakespeare.
-            return worksStream
-                // Map each work to a WorkSearchTask and fork it to
-                // run in parallel.
-                .map(work
-                     -> new WorkSearchTask(work).fork())
-
-                // Join with each forked task.
-                .map(ForkJoinTask::join)
-                
-                // Trigger intermediate operation processing and
-                // return a list.
-                .collect(toList());
-            */
-
-            // Create a list of WorkSearchTasks.
-            List<WorkSearchTask> worksList = worksStream
-                // Map each work of Shakespeare in the list into a
-                // new WorkSearchTask.
-                .map(WorkSearchTask::new)
-
-                // Trigger intermediate operation processing and
-                // return a list.
-                .collect(toList());
-
-            // Return a list of lists of search results - one list for
-            // each work of Shakespeare.  The invokeAll() method forks
-            // all the tasks in the worksList and returns a collection
-            // once all the tasks are done.
-            return invokeAll(worksList)
-                // Convert the list into a stream.
-                .stream()
-
-                // Join all the WorkSearchTask results.
-                .map(RecursiveTask::join)
-
-                // Trigger intermediate operation processing and
-                // return a list.
-                .collect(toList());
+            // Return the results.
+            return results;
         }
     }
         
