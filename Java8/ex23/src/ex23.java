94ba83dbf34a4fd4be1b6a1f1fd696490e2426e8
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2017-11-17 11:04:26 -0600

    updates

--- Java8/ex23/src/ex23.java
+++ Java8/ex23/src/ex23.java
@@ -13,11 +13,22 @@ import static java.util.stream.Collectors.toList;
 import static utils.FuturesCollectorStream.toFuture;
 
 /**
- * This example shows ...
+ * This example shows the difference between calling join() on
+ * intermediate completable futures (which block and thus degrade
+ * performance) vs. simply making one call to join() (and thus
+ * enhancing greater parallelism).
  */
 public class ex23 {
+    /**
+     * A counter used to monotonically/atomically generate a new value
+     * each time it's called.
+     */
     private static AtomicInteger mCounter = new AtomicInteger(0);
 
+    /**
+     * This supplier increments the atomic counter, sleeps for 1
+     * second, and then returns the incremented value.
+     */
     private static Supplier<Integer> mSupplier = () -> {
         int result = mCounter.incrementAndGet();
         display("enter Supplier with value "
@@ -31,6 +42,10 @@ public class ex23 {
         return result;
     };
 
+    /**
+     * This function returns even numbers passed to it and null if an
+     * odd number is passed to it.
+     */
     private static Function<Integer, Integer> mAction = i -> {
         display("enter Function with value "
                 + i);
@@ -42,61 +57,111 @@ public class ex23 {
      * Main entry point into the test program.
      */
     public static void main (String[] argv) throws IOException {
+        // A list of suppliers that return integers.
         List<Supplier<Integer>> suppliers =
             Arrays.asList(mSupplier,
                           mSupplier,
                           mSupplier,
                           mSupplier);
 
+        // Run a test that makes multiple blocking join calls on
+        // completable futures.
         RunTimer.timeRun(() -> testManyJoins(suppliers),
                          "testManyJoins()");
 
+        // Run a test that makes just one blocking join calls on
+        // completable futures.
         RunTimer.timeRun(() -> testOneJoin(suppliers),
                          "testOneJoin()");
 
+        // Print the results.
         System.out.println(RunTimer.getTimingResults());
     }
 
+    /**
+     * This test makes multiple blocking join calls when processing a
+     * list of completable futures.
+     */
     private static void testManyJoins(List<Supplier<Integer>> suppliers) {
         System.out.println(">>> testManyJoins()");
 
+        // A future to a stream of integer results.
         CompletableFuture<Stream<Integer>> resultsFuture = suppliers
+            // Convert the list of suppliers into a stream of suppliers.
             .stream()
+
+            // Run each supplier asynchronously.
             .map(CompletableFuture::supplyAsync)
+
+            // Filter out any null results.  The join() method causes
+            // the call to block.
             .filter(intFuture -> intFuture.thenApply(mAction).join() != null)
+
+            // Trigger intermediate operations and return a future to
+            // a stream of results.
             .collect(toFuture());
 
         resultsFuture
+            // When all the futures associated with resultsFuture
+            // complete then display the results.
             .thenAccept(stream ->
                         display("results = "
                                 + stream
+                                // Trigger intermediate processing and
+                                // return a list of results.
                                 .collect(toList())))
+
+            // Block caller until all processing is complete.
             .join();
+
         System.out.println("<<< leave testManyJoins()");
     }
 
+    /**
+     * This test makes just one blocking join call when processing a
+     * list of completable futures.
+     */
     private static void testOneJoin(List<Supplier<Integer>> suppliers) {
         System.out.println(">>> enter testOneJoin()");
 
+        // A future to a stream of integer results.
         CompletableFuture<Stream<Integer>> resultFuture = suppliers
+            // Convert the list of suppliers into a stream of suppliers.
             .stream()
+
+            // Run each supplier asynchronously.
             .map(CompletableFuture::supplyAsync)
+
+            // Asynchronously apply the action to the results of the
+            // previous completion stage.
             .map(intFuture -> intFuture.thenApply(mAction))
+
+            // Trigger intermediate operations and return a future to
+            // a stream of results.
             .collect(toFuture());
 
         resultFuture
+            // When all the futures associated with resultsFuture
+            // complete then display the results.
             .thenAccept(stream -> 
                         display("results = " 
                                 + stream
+                                // Filter out any null results.
                                 .filter(Objects::nonNull)
+
+                                // Trigger intermediate processing and
+                                // return a list of results.
                                 .collect(toList())))
+
+            // Block caller until all processing is complete.
             .join();
 
         System.out.println("<<< leave testOneJoin()\n");
     }
 
     /**
-     * Display the {@code string} after prepending the thread id.
+     * Display the {@code string} after prepending the thread id and
+     * curren time.
      */
     private static void display(String string) {
         System.out.println("["
