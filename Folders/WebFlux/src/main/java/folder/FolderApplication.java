725cc94ccc91ceb4879c7f596d985b16ec389548
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2020-05-09 14:08:12 -0500

    updates

--- Folders/WebFlux/src/main/java/folder/FolderApplication.java
+++ Folders/WebFlux/src/main/java/folder/FolderApplication.java
@@ -1,3 +1,5 @@
+package folder;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- Folders/WebFlux/src/main/java/folder/controller/FolderController.java
+++ Folders/WebFlux/src/main/java/folder/controller/FolderController.java
@@ -1,4 +1,4 @@
-package webfolder.controller;
+package folder.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
