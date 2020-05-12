1168739ef071ae619425b9d0e13d483c11955390
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2020-05-11 21:17:36 -0500

    updates

--- Reactor/ex4/src/main/java/ex4.java
+++ Reactor/ex4/src/main/java/ex4.java
@@ -26,13 +26,14 @@ import static java.util.stream.Collectors.toMap;
  * This program applies WebFlux and Project Reactor features to
  * implement various types of backpressure strategies (e.g., ignore,
  * buffer, error, latest, drop, and push/pull) between a publisher
- * that runs as a micro-service in one process and a subscriber that
- * runs in one or more threads in a different process.  This program
- * also measures the performance of checking random numbers for
- * primality with and without various types of memoizers (e.g.,
- * untimed and timed) based on Java ConcurrentHashMap.  In addition,
- * it demonstrates the use of slicing with the Flux takeWhile() and
- * skipWhile() operations.
+ * that runs as a micro-service in one process and produces a flux
+ * stream of random integers and a subscriber that runs in one or more
+ * threads in a different process and consumes this stream of
+ * integers.  This program also measures the performance of checking
+ * these random numbers for primality with and without various types
+ * of memoizers (e.g., untimed and timed) based on Java
+ * ConcurrentHashMap.  In addition, it demonstrates the use of slicing
+ * with the Flux takeWhile() and skipWhile() operations.
  */
 public class ex4 {
     /**
