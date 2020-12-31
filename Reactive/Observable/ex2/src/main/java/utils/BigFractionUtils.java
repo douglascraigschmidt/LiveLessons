12c5f5d3eb70216eae0e5f2f640bad72551b3873
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2020-12-31 09:01:37 -0600

    updates

--- Reactive/Observable/ex2/src/main/java/utils/BigFractionUtils.java
+++ Reactive/Observable/ex2/src/main/java/utils/BigFractionUtils.java
@@ -17,41 +17,6 @@ public class BigFractionUtils {
     }
 
     /**
-     * Number of big fractions to process asynchronously in a Reactor
-     * flux stream.
-     */
-    public static final int sMAX_FRACTIONS = 10;
-
-    /**
-     * These final strings are used to pass params to various lambdas in the
-     * test methods below.
-     */
-    public static final String sF1 = "62675744/15668936";
-    public static final String sF2 = "609136/913704";
-    public static final String sBI1 = "846122553600669882";
-    public static final String sBI2 = "188027234133482196";
-
-    /**
-     * A big reduced fraction constant.
-     */
-    public static final BigFraction sBigReducedFraction =
-        BigFraction.valueOf(new BigInteger("846122553600669882"),
-                            new BigInteger("188027234133482196"),
-                            true);
-
-    /**
-     * Stores a completed mono with a value of sBigReducedFraction.
-     */
-    public static final Single<BigFraction> mBigReducedFractionS =
-        Single.just(sBigReducedFraction);
-
-    /**
-     * Represents a test that's completed running when it returns.
-     */
-    public static final Completable sVoid =
-        Completable.complete();
-
-    /**
      * Display the {@code string} after prepending the thread id.
      */
     public static void display(String string) {
