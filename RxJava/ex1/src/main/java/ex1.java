329c76f5360590a4375f6667a87ce9fe305d5847
douglascraigschmidt <d.schmidt@vanderbilt.edu>
2021-02-01 11:53:42 -0600

    updates

--- RxJava/ex1/src/main/java/ex1.java
+++ RxJava/ex1/src/main/java/ex1.java
@@ -4,21 +4,36 @@ import io.reactivex.rxjava3.core.Single;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
 /**
  */
 public class ex1 {
-    static public void main(String[] argv) throws InterruptedException {
-        List<String> l1 = Arrays.asList("1","2","3");
-        
-        Single<List<String>> l2 = Observable
-            .fromIterable(l1)
-            .repeat(3)
-            .collect(toList());
+    static List<String> concatRxJava(List<String> list, int n) {
+        return Observable
+                .fromIterable(list)
+                .repeat(n)
+                .collect(toList())
+                .blockingGet();
+    }
 
-        System.out.println(l2.blockingGet());
+    static List<String> concatStream(List<String> list, int n) {
+        Stream<String> s = Stream.empty();
+
+        while (--n >= 0)
+            s = Stream.concat(s, list.stream());
+
+        return s.collect(toList());
+
+    }
+
+    static public void main(String[] argv) throws InterruptedException {
+        List<String> list = Arrays.asList("1","2","3");
+
+        System.out.println(concatRxJava(list, 3));
+        System.out.println(concatStream(list, 3));
     }
 }
 
