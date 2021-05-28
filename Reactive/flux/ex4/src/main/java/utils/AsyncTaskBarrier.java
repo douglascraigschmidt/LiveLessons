7a84f856b37c933edf2e11008a80d74111f5f261
douglascraigschmidt <douglas.craig.schmidt@gmail.com>
2021-05-28 17:57:48 -0500

    updates

--- Reactive/flux/ex4/src/main/java/utils/AsyncTaskBarrier.java
+++ Reactive/flux/ex4/src/main/java/utils/AsyncTaskBarrier.java
@@ -8,10 +8,27 @@ import java.util.List;
 import java.util.function.Supplier;
 
 /**
- * This class asynchronously runs tasks that use the Project Reactor
- * framework and ensures that the calling method doesn't exit until
- * all asynchronous task processing is completed.
+ * This class runs (a)synchronous tasks using the Project Reactor
+ * framework.  It can also be used to ensure the calling method
+ * doesn't exit until all (a)synchronous task processing completes.
+ *
+ * <p><b>Sample Usage.</b> The following code snippet illustrates a
+ * common idiom for using the {@code AsyncTaskBarrier} class.<br>
+ *
+ * <pre>{@code
+ *   // Register asynchronous tasks under test.
+ *   AsyncTaskBarrier.register(FluxEx::testFractionMultiplicationsAsync1);
+ *   AsyncTaskBarrier.register(FluxEx::testFractionMultiplicationsAsync2);
+ *
+ *   long testCount = AsyncTaskBarrier
+ *     // Run all the tasks.
+ *     .runTasks()
  * 
+ *     // Block until all asynchronous processing is done.
+ *     .block();
+ *
+ *   System.out.println("Completed " + testCount + " tests");
+ * }}</pre>
  */
 public class AsyncTaskBarrier {
     /**
@@ -21,10 +38,9 @@ public class AsyncTaskBarrier {
         new ArrayList<>();
 
     /**
-     * Register the {@code task} task so that it will be run
-     * asynchronously when {@code runTasks()} is called.  Each task
-     * must take no parameters and return a {@code
-     * Supplier<Mono<Void>>} result.
+     * Register the {@code task} so it runs (a)synchronously when
+     * {@code runTasks()} is called.  Each task takes no parameters
+     * and returns a {@code Supplier<Mono<Void>>} result.
      * 
      * @param task The task to register with {@code AsyncTaskBarrier}
      */
@@ -35,9 +51,8 @@ public class AsyncTaskBarrier {
 
     /**
      * Unregister the {@code task} task so that it is no longer run
-     * asynchronously when {@code runTasks()} is called.  Each task
-     * must take no parameters and return a {@code
-     * Supplier<Mono<Void>>} result.
+     * when {@code runTasks()} is called.  Each task must take no
+     * parameters and return a {@code Supplier<Mono<Void>>} result.
      * 
      * @param task The task to unregister with {@code AsyncTaskBarrier}
      * @return True if {@code task} was previously registered, else false.
@@ -46,12 +61,11 @@ public class AsyncTaskBarrier {
         return sTasks.remove(task);
     }
 
-
     /**
      * Run all the register tasks.
      *
      * @return a {@code Mono<Long>} that will be triggered when all
-     * the asynchronously-run tasks complete to indicate how many
+     * the (a)synchronously-run tasks complete to indicate how many
      * tasks were run.
      */
     public static Mono<Long> runTasks() {
@@ -59,18 +73,17 @@ public class AsyncTaskBarrier {
             // Factory method that converts the list into a flux.
             .fromIterable(sTasks)
 
-            // Run each task, which can execute asynchronously.
+            // Run each task, which can execute (a)synchronously.
             .flatMap(Supplier::get)
 
             // Collect into an empty list that triggers when all
-            // the tasks finish running asynchronously.
+            // the tasks finish running (a)synchronously.
             .collectList()
 
             // Return a mono containing the number of tasks run when
-            // we're done.
+            // they all complete.
             .flatMap(l -> Mono
                      // Use just() to return the number of tasks run.
                      .just((long) sTasks.size()));
     }
 }
-
