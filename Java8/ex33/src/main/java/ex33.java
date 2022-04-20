94cf7e2b6f523aabf25fb9d44370839481f7a05b
Douglas C. Schmidt <d.schmidt@vanderbilt.edu>
2022-04-20 11:24:46 -0500

    review comments

--- Java8/ex33/src/main/java/ex33.java
+++ Java8/ex33/src/main/java/ex33.java
@@ -1,18 +1,12 @@
-import io.reactivex.rxjava3.annotations.NonNull;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Single;
+import managedblockers.ManagedLocker;
+import managedblockers.QueueTaker;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toList;
 import static utils.ExceptionUtils.rethrowRunnable;
 
 /**
@@ -22,93 +16,6 @@ import static utils.ExceptionUtils.rethrowRunnable;
  * https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ForkJoinPool.ManagedBlocker.html
  */
 public class ex33 {
-    /**
-     * Implements a ManagedBlocker that potentially blocks until a
-     * {@link ReentrantLock} is available.
-     */
-    static class ManagedLocker
-           implements ForkJoinPool.ManagedBlocker {
-        /**
-         * The {@link ReentrantLock}.
-         */ 
-        final Lock mLock;
-
-        /**
-         * Keeps track of whether the lock was obtained.
-         */
-        boolean mHasLock = false;
-
-        /**
-         * Constructor initializes the field.
-         */
-        ManagedLocker(Lock lock) {
-            mLock = lock; 
-        }
-
-        /**
-         * Tries to obtain the lock if it's immediately available, but
-         * doesn't block if it's not available.
-         */
-        public boolean isReleasable() {
-            return mHasLock || (mHasLock = mLock.tryLock()); 
-        }
-
-        /**
-         * Block until the lock is available and lock it.
-         */
-        public boolean block() {
-            if (!mHasLock) 
-                mLock.lock();
-            return true;  
-        } 
-    }
-
-    /**
-     * Implements a ManagedBlocker that potentially blocks until an
-     * item is available in a {@link BlockingQueue}.
-     */
-    static class QueueTaker<E> 
-           implements ForkJoinPool.ManagedBlocker {
-        /**
-         * The {@link BlockingQueue}.
-         */ 
-        final BlockingQueue<E> mQueue;
-
-        /**
-         * The item obtained from the {@link BlockingQueue}.
-         */
-        volatile E mItem = null;
-
-        /**
-         * Constructor initializes the field.
-         */
-        QueueTaker(BlockingQueue<E> q) { mQueue = q; }
-
-        /**
-         * Tries to obtain an item from the queue if it's immediately
-         * available, but doesn't block if it's not available.
-         */
-        public boolean isReleasable() { 
-            return mItem != null || (mItem = mQueue.poll()) != null; 
-        }
-
-        /**
-         * Block until the item is available on the queue.
-         */
-        public boolean block() throws InterruptedException {
-            if (mItem == null)
-                mItem = mQueue.take();
-            return true; 
-        }
-
-        /**
-         * @return Return the item on the queue
-         */
-        public E getItem() { 
-            return mItem; 
-        }
-    }
-
     /**
      * Main entry point into the test program.
      */
