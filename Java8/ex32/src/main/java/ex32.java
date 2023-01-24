bce3c8b1311459091cf2b7bc54224a5c5b37811b
Douglas C. Schmidt <d.schmidt@vanderbilt.edu>
2023-01-24 12:49:29 -0600

    review comments

--- Java8/ex32/src/main/java/ex32.java
+++ Java8/ex32/src/main/java/ex32.java
@@ -17,15 +17,71 @@ public class ex32 {
      * The number of times to concatenate.
      */
     private static final int sMAX_CONCAT = 3;
+
+    /**
+     * The number of random {@link Integer} objects to generate.
+     */
     private static final long sMAX_INTs = 5;
+
+    /**
+     * The lower bound of the randomly generated {@link Integer}
+     * objects.
+     */
     private static final int sLOWER_BOUND = 1;
+
+    /**
+     * The upper bound of the randomly generated {@link Integer}
+     * objects.
+     */
     private static final int sUPPER_BOUND = 10;
+
+    /**
+     * Use the Java Stream concat() method to concatenate the contents
+     * of {@code list} together {@code n} times.
+     */
+    static List<String> concatStream1(List<String> list,
+                                      int n) {
+        // Create an empty stream.
+        Stream<String> s = Stream.empty();
+
+        while (--n >= 0)
+            // Concatenate the contents of the List to the
+            // end of the Stream n times.
+            s = Stream.concat(s, list.stream());
+
+        // Collect the results into a List and return it.
+        return s.toList();
+    }
+
+    /**
+     * Use the Java Stream {@code mapToObj()} and {@code flatMap()}
+     * methods to concatenate the contents of {@code list} together
+     * {@code n} times.
+     */
+    static List<String> concatStream2(List<String> list,
+                                      int n) {
+        return IntStream
+            // Create a stream of integers of size n.
+            .rangeClosed(1, n)
+
+            // Repeatedly emit the list 'n' times, yielding a Stream
+            // of List of String objects.
+            .mapToObj(__ -> list)
+
+            // Flatmap the Stream of List of String objects into a
+            // single Stream of String objects.
+            .flatMap(List::stream)
+
+            // Collect the results into a List of Strings.
+            .toList();
+    }
 
     /**
      * Use the RxJava repeat() method to concatenate the contents of
      * {@code list} together {@code n} times.
      */
-    static List<String> concatRxJava(List<String> list, int n) {
+    static List<String> concatRxJava(List<String> list,
+                                     int n) {
         return Observable
             // Convert the List into an Observable.
             .fromIterable(list)
@@ -41,56 +97,18 @@ public class ex32 {
     }
 
     /**
-     * Use the Java Stream concat() method to concatenate the contents
-     * of {@code list} together {@code n} times.
-     */
-    static List<String> concatStream1(List<String> list, int n) {
-        // Create an empty stream.
-        Stream<String> s = Stream.empty();
-
-        while (--n >= 0)
-            // Concatenate the contents of the List to the
-            // end of the Stream n times.
-            s = Stream.concat(s, list.stream());
-
-        // Collect the results into a List and return it.
-        return s.toList();
-    }
-
-    /**
-     * Use the Java Stream mapToObj(), flatMap() methods to concatenate the
-     * contents of {@code list} together {@code n} times.
-     */
-    static List<String> concatStream2(List<String> list, int n) {
-        return IntStream
-            // Create a stream of integers of size n.
-            .rangeClosed(1, n)
-
-            // Repeatedly emit the list 'n' times, yielding
-            // a Stream of List of String objects.
-            .mapToObj(__ -> list)
-
-            // Flatmap the Stream of List of String objects
-            // into a single Stream of String objects.
-            .flatMap(List::stream)
-
-            // Collect the results into a List of Strings.
-            .toList();
-    }
-
-    /**
      * Main entry point into the test program.
      */
     static public void main(String[] argv) throws InterruptedException {
         // Create a List of random Integers.
         var list = getRandomInts(sMAX_INTs,
-                                          sLOWER_BOUND,
-                                          sUPPER_BOUND);
+                                 sLOWER_BOUND,
+                                 sUPPER_BOUND);
 
         // Perform all the concatenations.
-        System.out.println(concatRxJava(list, sMAX_CONCAT));
         System.out.println(concatStream1(list, sMAX_CONCAT));
         System.out.println(concatStream2(list, sMAX_CONCAT));
+        System.out.println(concatRxJava(list, sMAX_CONCAT));
     }
 }
 
