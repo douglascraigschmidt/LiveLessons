804f089dca5beec15d97916a234602fbc7345fc9
Douglas C. Schmidt <d.schmidt@vanderbilt.edu>
2023-01-31 08:36:18 -0600

    review comments

--- Java8/ex24/src/main/java/utils/ExampleCountDownTimer.java
+++ Java8/ex24/src/main/java/utils/ExampleCountDownTimer.java
@@ -3,6 +3,10 @@ package utils;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.locks.Lock;
 
+/**
+ * An example {@link CountDownTimer} that displays a text string on
+ * each {@code onTick()} and {@code onFinish()} call.
+ */ 
 public class ExampleCountDownTimer
       extends CountDownTimer {
     /**
@@ -10,6 +14,18 @@ public class ExampleCountDownTimer
      */
     private final CountDownLatch mCdl;
 
+    /**
+     * @param lock The {@link Lock} used to synchronize access to the
+     *             object
+     * @param millisInFuture The number of millis in the future from
+     *                       the call to {@link #start()} until the
+     *                       countdown is done and {@link #onFinish()}
+     *                       is called
+     * @param countDownInterval The interval along the way to receive
+     *                          {@link #onTick(long)} callbacks
+     * @param cdl The countDownLatch used to wait until the test is
+     *            done
+     */
     public ExampleCountDownTimer(Lock lock,
                                  long millisInFuture,
                                  long countDownInterval,
@@ -20,18 +36,20 @@ public class ExampleCountDownTimer
 
     /**
      * Callback fired on regular interval.
-
+     *
      * @param millisUntilFinished The amount of time until finished
      */
     @Override
     public void onTick(long millisUntilFinished) {
+        // Display a result to the user.
         System.out.println("seconds remaining: " + 
                            millisUntilFinished / 1000);
-        // Try to cancel the timer after it's about halfway
-        // done.
-        if ((mMillisInFuture - millisUntilFinished) > millisUntilFinished) {
-            // This call will trigger self-deadlock if a
-            // non-reentrant lock is used.
+
+        // Try to cancel the timer after it's roughly halfway done.
+        if ((mMillisInFuture - millisUntilFinished) 
+            > millisUntilFinished) {
+            // This call will trigger self-deadlock if a non-reentrant
+            // lock is used.
             this.cancel();
 
             // Decrement the latch to release the waiter.
