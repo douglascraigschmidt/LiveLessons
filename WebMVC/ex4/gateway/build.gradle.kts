b26c10ba369e016f5435017b71208ec306fbe798
Douglas C. Schmidt <d.schmidt@vanderbilt.edu>
2023-02-25 08:40:42 -0600

    review comments

--- WebMVC/ex4/gateway/build.gradle.kts
+++
@@ -1,4 +0,0 @@
-dependencies {
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
-    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-}
