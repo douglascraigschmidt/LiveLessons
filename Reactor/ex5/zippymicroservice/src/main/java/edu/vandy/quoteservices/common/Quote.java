284600ec5bd35b8663157e4f841e7c7dc3aad38d
Douglas C. Schmidt <d.schmidt@vanderbilt.edu>
2023-03-13 13:55:13 -0500

    review comments

--- Reactor/ex5/zippymicroservice/src/main/java/edu/vandy/quoteservices/common/Quote.java
+++ Reactor/ex5/zippymicroservice/src/main/java/edu/vandy/quoteservices/common/Quote.java
@@ -4,7 +4,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import jakarta.persistence.Entity;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.Id;
+// import org.springframework.data.annotation.Id;
 
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
